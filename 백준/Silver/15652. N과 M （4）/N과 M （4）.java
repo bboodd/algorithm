588c7f4915b9b1a91b4f93b0c10b26542e51bc79
Hakhyeon Kim <gkrgus9@gmail.com>
2023-12-22 18:50:19 +0900

    [Silver III] Title: N과 M (4), Time: 208 ms, Memory: 21156 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    //n과 m(4)
+
+    public static int n,m;
+    public static int[] arr;
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[m];
+
+        dfs(0);
+        System.out.print(sb);
+    }
+
+    public static void dfs(int depth){
+
+        if(depth == m){
+            for(int i=0;i<m;i++){
+                sb.append(arr[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=1;i<=n;i++){
+
+            if(depth >= 1){
+                if(arr[depth-1] <= i){
+                    arr[depth] = i;
+                    dfs(depth + 1);
+                }
+            } else{
+                arr[depth] = i;
+                dfs(depth + 1);
+            }
+        }
+    }
+}
