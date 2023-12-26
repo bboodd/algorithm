e41e1f96e8123010ff32ae131ca6365a7b9479cc
Hakhyeon Kim <gkrgus9@gmail.com>
2023-12-26 22:35:44 +0900

    [Bronze IV] Title: 심부름 가는 길, Time: 124 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/5554. 심부름 가는 길/심부름 가는 길.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int sum = 0;
+
+        for(int i=0;i<4;i++) {
+            int n = Integer.parseInt(br.readLine());
+            sum += n;
+        }
+
+        System.out.println(sum/60);
+        System.out.println(sum%60);
+    }
+}
