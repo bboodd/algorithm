e0100ab55cd0d4b2c43bf3a002a1b3927dd7f5c4
Hakhyeon Kim <gkrgus9@gmail.com>
2024-01-18 22:10:25 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 136 ms, Memory: 14520 KB -BaekjoonHub

--- 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.java
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.java
@@ -1,51 +1,60 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String s = "";
-        while(true){
-            s = sc.next();
-            if(s.equals("end"))break;
-            boolean mFlag = false;
-            boolean eoFlag = true;
-            boolean jmFlag = true;
-            int mCnt = 0;
-            int jCnt = 0;
+    //비밀번호 발음하기
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        while(true) {
+            String word = br.readLine();
 
+            if(word.equals("end")) break;
 
-            for(int i = 0 ; i< s.length(); i++){
-                if(s.charAt(i) == 'a' || s.charAt(i) == 'e' ||s.charAt(i) == 'i' ||s.charAt(i) == 'o' ||s.charAt(i) == 'u'){
-                    mFlag = true;
-                    mCnt++;
-                    jCnt = 0;
-                    if(mCnt >= 3){
-                        jmFlag = false;
-                        break;
-                    }
-                }else{
-                    jCnt++;
-                    mCnt = 0;
-                    if(jCnt >= 3){
-                        jmFlag = false;
-                        break;
-                    }
+            boolean[] mo = new boolean[word.length()];
+
+            int wrong = 0;
+
+            for(int i=0; i<word.length(); i++) {
+                if(word.charAt(i) == 'a' || word.charAt(i) == 'e'
+                || word.charAt(i) == 'i' || word.charAt(i) == 'o'
+                || word.charAt(i) =='u'){
+                    mo[i] = true;
                 }
 
-                if(i >= 1){
-                    if(s.charAt(i) != 'e' && s.charAt(i) != 'o'){
-                        if(s.charAt(i) == s.charAt(i-1)) eoFlag = false;
+                if(i>0){
+                    if(word.charAt(i) == word.charAt(i-1)){
+                        if(word.charAt(i) == 'e' || word.charAt(i) == 'o'){
+                            continue;
+                        } else{
+                            wrong = 1;
+                        }
                     }
                 }
             }
 
-            if(jmFlag && eoFlag && mFlag){
-                System.out.println("<" + s + "> is acceptable.");
-            }else{
-                System.out.println("<" + s + "> is not acceptable.");
+            int cnt = 0;
+            for(int i=0;i<mo.length;i++){
+                if(mo[i]) cnt++;
+
+                if(i>1){
+                    if(mo[i] == mo[i-1] && mo[i-1] == mo[i-2]){
+                        wrong = 1;
+                    }
+                }
             }
+
+            if(cnt == 0){
+                wrong = 1;
+            }
+
+            if(wrong == 1){
+                System.out.printf("<%s> is not acceptable.", word);
+            } else{
+                System.out.printf("<%s> is acceptable.", word);
+            }
+            System.out.println();
         }
     }
-
 }
