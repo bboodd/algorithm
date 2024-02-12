d14bdbe92e7c5cf1fed26828e0ec74a5f3acb98a
Hakhyeon Kim <gkrgus9@gmail.com>
2024-02-12 23:04:54 +0900

    [Bronze IV] Title: Darius님 한타 안 함?, Time: 124 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/20499. Darius님 한타 안 함？/Darius님 한타 안 함？.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    //Darius님 한타 안 함?
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String[] kda = br.readLine().split("/");
+        
+        int k = Integer.parseInt(kda[0]);
+        int d = Integer.parseInt(kda[1]);
+        int a = Integer.parseInt(kda[2]);
+        
+        if(k+a < d || d == 0){
+            System.out.println("hasu");
+        } else{
+            System.out.println("gosu");
+        }
+
+
+    }
+}
