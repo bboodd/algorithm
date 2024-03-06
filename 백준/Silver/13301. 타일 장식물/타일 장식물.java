60371cb5fd09f072fa86e54136525a44eb1ac726
Hakhyeon Kim <gkrgus9@gmail.com>
2024-03-06 20:02:44 +0900

    [Silver V] Title: 타일 장식물, Time: 128 ms, Memory: 14220 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/13301. 타일 장식물/타일 장식물.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+  public static void main(String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    int n = Integer.parseInt(br.readLine());
+    
+    long[] arr = new long[n+1];
+    
+    arr[0] = 1;
+    arr[1] = 1;
+    
+    for(int i=2;i<=n;i++){
+    arr[i] = arr[i-2] + arr[i-1];
+    }
+    
+    if(n == 1){System.out.println(4);}
+    else{System.out.println((arr[n-1]*4) + (arr[n-2]*2));}
+  
+  }
+  
+}
