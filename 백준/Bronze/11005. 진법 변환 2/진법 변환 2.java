16485ff3d6ceb565330a624014ed306238475019
Hakhyeon Kim <gkrgus9@gmail.com>
2024-04-14 22:43:23 +0900

    [Bronze I] Title: 진법 변환 2, Time: 124 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int radix = Integer.parseInt(st.nextToken());
+
+        System.out.println(Integer.toString(n,radix).toUpperCase());
+    }
+}
